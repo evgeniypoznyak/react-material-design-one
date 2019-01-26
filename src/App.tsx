a50d29fefda7aec19a53f57af022db5fd31c0188
Evgeniy Poznyak <ek35mm@gmail.com>
2019-01-25 19:38:59 -0600

    Material Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Button variant="raised" color="primary">
+            Hello World
+          </Button>
         </header>
       </div>
     );
